ff95e55c11dbaa24c2fd102346c73479387497a8
Akhilesh Srivastava <akhil110@gmail.com>
2020-08-12 11:15:41 +0530

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,10 +7,14 @@ const routes: Routes = [
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   },
   {
+    path: 'about',
+    loadChildren: () => import('./about/about.module').then( m => m.AboutPageModule)
+  },
+  {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
-  },
+  }
 ];
 
 @NgModule({
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,68 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { ToastController } from '@ionic/angular';
+import { IonReorderGroup } from '@ionic/angular';
+import { ModaldialogComponent } from '../modaldialog/modaldialog.component';
+import { StorageService } from '../service/storage.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  constructor(
+    public modalController: ModalController,
+    public toastController: ToastController,
+    private db: StorageService
+  ) {}
+  tasks: any = [];
 
-  constructor() {}
+  @ViewChild(IonReorderGroup, { static: true }) reorderGroup: IonReorderGroup;
+
+  ngOnInit() {
+    this.tasks = this.db.getTasklist();
+  }
+
+  async showModal() {
+    const modal = await this.modalController.create({
+      component: ModaldialogComponent
+    });
+    return await modal.present();
+  }
+
+  async updateTask(id: number) {
+    const modal = await this.modalController.create({
+      component: ModaldialogComponent,
+      componentProps: {
+        taskId: id
+      }
+    });
+    const slidingItem = document.getElementById('slidingItem') as any;
+    slidingItem.closeOpened();
+    return await modal.present();
+  }
+
+  removeTask(id: number) {
+    this.db.delTask(id);
+    this.removeToast();
+    this.tasks = this.db.getTasklist();
+  }
+
+  async removeToast() {
+    const toast = await this.toastController.create({
+      message: 'Task removed',
+      color: 'danger',
+      duration: 2000
+    });
+    toast.present();
+  }
+
+  changeStatus(id: number, status: string) {
+    this.db.taskStatus(id, status);
+  }
+  reorderList(ev: any) {
+    ev.detail.complete();
+  }
 
 }
